f3e1a12b7c798c9c668997248612171b27d017c9
OMNIA93 <120645154+OMNIA93@users.noreply.github.com>
2023-09-24 15:24:35 +0300

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
 
+/**
+ * swap_int - swaps two elements in an array
+ * @a: the first element
+ * @b: the second element
+ */
+
 void swap(int *a, int *b) {
 int temp = *a;
 *a = *b;
 *b = temp;
 }
+
+/**
+ * lomuto_partition - partition through an array of integers
+ * @array: array of integers
+ * @size: the size of the array
+ * @l: first index of array
+ * @h: last index of array
+ *
+ * Description: all values must be accurate
+ * Return: new index position
+ */
 
 int lomuto_partition(int *array, int low, int high) {
 int pivot = array[high];
@@ -21,6 +38,17 @@ swap(&array[i + 1], &array[high]);
 return i + 1;
 }
 
+/**
+ * quicksort_recursive - Recursive helper function for the Quick sort algorithm
+ *
+ * @array: Pointer to the first element of the array
+ * @low: Index of the low partition
+ * @high: Index of the high partition
+ * @size: Number of elements in the array
+ *
+ * Return: Nothing
+ */
+
 void quick_sort_recursive(int *array, int low, int high) {
 if (low < high) {
 int partition_index = lomuto_partition(array, low, high);
@@ -35,6 +63,16 @@ quick_sort_recursive(array, low, partition_index - 1);
 quick_sort_recursive(array, partition_index + 1, high);
 }
 }
+
+/**
+ * quick_sort - Sorts an array of integers in ascending order using the Quick
+ *              sort algorithm with the Lomuto partition scheme
+ *
+ * @array: Pointer to the first element of the array
+ * @size: Number of elements in the array
+ *
+ * Return: Nothing
+ */
 
 void quick_sort(int *array, size_t size) {
 if (size < 2) {
